d97cfcd07d424618f84eced743f39501ed6c9a89
刘奇 <qi.liu@irootech.com>
2019-04-19 18:10:00 +0800

    专题浏览d页面优化

--- codelife-ui/src/components/topic/TopicExplorer.vue
+++ codelife-ui/src/components/topic/TopicExplorer.vue
@@ -1,86 +1,48 @@
 <!-- 专题浏览 -->
 <template>
-  <div class="container-fluid">
-    <!--搜索操作-->
-    <form method="get" th:action="@{/topic/search}"
-          class="row m-4">
+  <div>
+    <div class="col">
+      <!--搜索操作-->
+      <form method="get"
+            class="row m-4">
         <input type="text"
-               placeholder="搜索专题" name="key" required
-               class="form-control col-md-4 mr-md-2 offset-md-3"/>
+              placeholder="搜索专题" name="key" required
+              class="form-control col-md-4 mr-md-2 offset-md-3"/>
         <input type="submit" value="搜索"
             class="btn btn-primary"/>
-    </form>
+      </form>
 
-    <div class="row topic-list" v-if="null != loginUser">
-        <!--我订阅的专题-->
-        <div class="topic-title-main col-12 mb-2">已订阅专题</div>
-        <div class="col-sm-6" v-for="topic in myTopics" :key="topic.id">
-            <div class="topic-block row">
-                <h6 class="col-sm-12 topic-block-title">
-                    <div class="row">
-                        <span class="col-sm-10">{{topic.title}}</span>
-                        <span class="col-sm-2 text-right topic-subscribe-button"
-                              onclick="'unSubscribe(' + ${topic.id} + ')'">取消订阅</span>
-                    </div>
-                </h6>
+      <div class="row">
+        <div class="col-sm-3 d-none d-sm-block">
+          <div class="block row ml-0 mr-0">
+            <font class="block-title col-sm">热门专题</font>
+          </div>
+        </div>
 
-                <div class="row">
-                    <div class="col-sm">
-                        <div class="row">
-                          <p class="col-sm">
-                            {{topic.introduction}}
-                          </p>
+        <div class="col-sm">
+          <div class="row topic-block" v-for="topic in topics" :key="topic.id">
+            <router-link class="col-sm-12 topic-block-title" :to="{name: 'topicExplorer'}">{{ topic.title }}</router-link>
 
-                          <ul class="col-sm-12 ml-4">
-                              <li v-for="article in topic.articles" :key="article.id">
-                                  <a th:href="@{/article/detail} + '?id=' + ${article.id}">{{article.title}}</a>
-                              </li>
-                          </ul>
-                        </div>
-                    </div>
+            <img class="col-sm-4 p-0" v-if="topic.img != ''"
+                :src="baseUrl + 'topic/getImg' + '?fileName=' + topic.img"/>
 
-                    <div class="col-sm-3 p-2">
-                      <img class="topic-block-img"
-                         v-bind:src="'/topic/getImg}?fileName=' + topic.img" width="200px" height="200px"/>
-                    </div>
-                </div>
+            <img class="col-sm-4 p-0" v-if="topic.img == ''"
+                height="200px"/>
 
-                <span class="row offset-md-10 col-md-2 text-right">
-                  <a th:href="@{/topic/detail} + '?id=' + ${topic.id}">>>查看更多</a></span>
+            <div class="col-sm m-0">
+              <p>{{topic.introduction}}</p>
+              <ul class="col-sm-12">
+                <li v-for="article in topic.articles" :key="article.id">
+                  <router-link class="article-item-title font-weight-bold"
+                    :to="{name: 'articleDetail', params: {id: article.id}}">{{ article.title }}</router-link>
+                </li>
+              </ul>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-
-    <div class="row p-1 topic-list">
-        <div class="col-12 mb-2 mt-4 topic-title-main">
-            专题浏览
-        </div>
-
-        <!--专题列表-->
-        <div class="col-sm-3" v-for="topic in topics" :key="topic.id">
-            <div class="row topic-block">
-                <h6 class="col-sm-12 topic-block-title">
-                    <div class="row">
-                        <span class="col-sm-10">{{topic.title}}</span>
-                        <span class="col-sm-2 text-right topic-subscribe-button"
-                              v-if="null != loginUser"
-                              th:onclick="'subscribe(' + ${topic.id} + ')'">订阅</span>
-                    </div>
-                </h6>
-
-                <p class="m-0">{{topic.introduction}}</p>
-
-                <ul class="col-12">
-                    <li v-for="article in topic.articles" :key="article.id">
-                        <a th:href="@{/article/detail} + '?id=' + ${article.id}">{{article.title}}</a>
-                    </li>
-                </ul>
-                <span class="offset-md-9"><a th:href="@{/topic/detail} + '?id=' + ${topic.id}">>>查看更多</a></span>
-            </div>
-        </div>
-
-    </div>
-</div>
+  </div>
 </template>
 
 <script>
@@ -92,7 +54,8 @@ export default {
   data () {
     return {
       topics: [], // 未订阅的专题清单
-      myTopics: [] // 我订阅的专题清单
+      myTopics: [], // 我订阅的专题清单
+      baseUrl: axios.defaults.baseURL
     }
   },
   computed: mapState([
@@ -115,13 +78,13 @@ export default {
 </script>
 
 <style>
-  .topic-title-main {
+ .topic-title-main {
     font-size: 1.1em;
     color: gray;
     border-bottom: 1px solid #f2f2f2;
   }
 
-    /*TOPIC清单中的TOPIC块*/
+  /*TOPIC清单中的TOPIC块*/
   .topic-block {
     margin-right: 1px;
     margin-bottom: 10px;
@@ -138,7 +101,7 @@ export default {
   }
 
   .topic-block img {
-    box-shadow: 1px 1px 1px #bfbfbf;
+    box-shadow: 2px 2px 2px #bfbfbf;
     border: 1px solid #efefef;
   }
 
@@ -154,6 +117,10 @@ export default {
 
   .topic-block ul li{
     margin: 4px;
+  }
+
+  .topic-block span{
+
   }
 
   /*订阅按钮样式*/
